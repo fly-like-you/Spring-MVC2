8764c5a6a940dc31fa914c052073c7905d174544
fly-like-you <qkrwnsgh71w@gmail.com>
2024-12-14 00:07:50 +0900

    섹션2. 속성 값 설정

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -63,6 +63,12 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/operation";
     }
+
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
     @Data
     static class User {
         private String username;
